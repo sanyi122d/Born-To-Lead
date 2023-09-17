8db9e5e7bf6f9eded794bc4d06c136867b5bcac4
sanyi122d <145164552+sanyi122d@users.noreply.github.com>
2023-09-17 23:25:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+"use  strict"
+
+ const forms = document.forms;
+ if(forms.length){
+     for (const form of forms){
+         const file = form.querySelector('input[ type="file"]')
+         file ? file.addEventListener('change', formAddfile) : null;
+         form.addEventListener('submit',formSubmitAction); 
+     }
+
+ }
+    function formAddfile(e) {
+        const formInputFile = e.target;
+        const  formFiles = formInputFile.files;  
+        const fileName = formFiles.length ? formFiles[0].name:'';
+        formInputFile.parentElement.nextElementSibling.innerHTML = fileName;  
+
+ }
+
+ async function formSubmitAction(e){
+     e.preventDefault();
+     const form = e.target;
+     const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : "#"; 
+     const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : "GET";
+     const formData = new FormData(form);
+
+      form.classList.add('from-sending');  
+
+   const response = await fetch(formAction,{
+       method:formMethod,
+       body:formData
+   })
+   if(response.ok){
+       alert('Form Sent!');
+       form.classList.remove('from-sending');  
+          
+       const formInputFile = form.querySelector('input[type="file"]');
+
+   formInputFile ? formInputFile.parentElement.nextElementSibling.innerHTML = '':null;
+   form.reset()  
+   
+   } else{
+       alert('Error')
+       form.classList.remove('form sending');
+ }
+ after. SubmitEvent  
+}
+let popup = document.getElementById("popup");
+
+ function openPopup(){
+     popup.classList.add("open-Popup")
+ }
+ function closePopup(){
+     popup.classList.remove("open-Popup")
+ }
+ 
